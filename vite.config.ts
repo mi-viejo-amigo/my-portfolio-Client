0d5a2151309fd571c39973560d1128d632e91c99
FROL <nkt.frlv7@yandex.ru>
2024-11-13 22:22:05 +0100

    vite.config chenges

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '../client', // Указываем корневую папку, чтобы Vite нашел index.html
+  root: './', // Указываем корневую папку, чтобы Vite нашел index.html
   build: {
     
     outDir: '../dist/client', // Папка для клиентской сборки
     ssrManifest: true,         // Для использования в SSR
+    rollupOptions: {
+      input: './index.html',  // Убедитесь, что путь к `index.html` указан правильно
+    }
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'socket.io-client'],
